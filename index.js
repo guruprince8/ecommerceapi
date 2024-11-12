33568d40dd60963a1e425513b4346996657f8eac
Gurubrahmanandam E <guru.prince8@gmail.com>
2024-11-11 23:42:31 -0600

    refactored index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const https = require('https')
-const fs = require('fs')
 const app = express();
-var log4js = require("log4js");
 require('dotenv').config();
-const appconfig = require('./config');
-const certOptions = require("./certs/loadCertificate");
 
+// load crt and key
+const certOptions = require("./certs/loadCertificate");
 
 // application routes
 const defaultRouter = require('./routes/default');
 const authenticateRouter = require('./routes/authenticate');
 const databaseRouter = require('./routes/database');
-app.use("/authenticate",authenticateRouter);
-app.use("/db",databaseRouter);
-app.use("/",defaultRouter);
+app.use("/authenticate", authenticateRouter);
+app.use("/db", databaseRouter);
+app.use("/", defaultRouter);
 
+// start https server on PORT mentioned in .env file
 var server = https.createServer(certOptions.options, app)
 server.listen(process.env.PORT, () => {
     console.log("server starting on port : " + process.env.PORT)
